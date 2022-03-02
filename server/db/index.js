fb6273d2f328a043141b43bf93b0c9eab46122bf
L Phansalkar (they/them) <n.e.phansalkar@gmail.com>
2022-03-02 11:24:43 -0500

    created cart db and associations

--- server/db/index.js
+++ server/db/index.js
@@ -4,15 +4,21 @@ const db = require('./db');
 
 const User = require('./models/User');
 const Poster = require('./models/Poster');
+const Cart = require('./models/Cart');
 
 //associations could go here!
 User.belongsToMany(Poster, { through: 'UserPoster' });
 Poster.belongsToMany(User, { through: 'UserPoster' });
+User.hasOne(Cart);
+Poster.belongsToMany(Cart, { through: 'CartPoster' });
+Cart.belongsToMany(Poster, { through: 'CartPoster' });
+
 
 module.exports = {
 	db,
 	models: {
 		User,
 		Poster,
+		Cart,
 	},
 };
