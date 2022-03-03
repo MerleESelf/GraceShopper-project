4d41cc77932baee895738d389ceecc88c4689299
Amber Lin <amberlinyq@gmail.com>
2022-03-02 19:51:29 -0500

    server/api/posters bug checked

--- server/api/posters.js
+++ server/api/posters.js
@@ -1,10 +1,19 @@
 // this should be merged with the allPosters API because they share the same /api/posters
 
-
 const router = require('express').Router()
 const { models: { Poster }} = require('../db')
 module.exports = router
 
+// route for all posters 
+router.get('/', async(req, res, next ) => {
+    try {
+        const allPosters = await Poster.findAll(); 
+        res.send(allPosters)
+    } catch (error) {
+        next(error)
+    }
+
+})
 router.get('/:id', async (req, res, next) => {
     try {
       const poster = await Poster.findByPk(req.params.id);
