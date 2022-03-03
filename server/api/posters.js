78518149202fe40c3bcf8d5aeabd70e928f83379
LPM <11pm@LPMs-MacBook-Air.local>
2022-03-03 11:54:49 -0500

    deleted some structure for structures

--- server/api/posters.js
+++ server/api/posters.js
@@ -1,31 +1,41 @@
 // this should be merged with the allPosters API because they share the same /api/posters
 
-const router = require('express').Router()
-const { models: { Poster }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { Poster },
+} = require("../db");
+module.exports = router;
 
-// route for all posters 
-router.get('/', async(req, res, next ) => {
-    try {
-        const allPosters = await Poster.findAll(); 
-        res.send(allPosters)
-    } catch (error) {
-        next(error)
-    }
-
-})
-router.get('/:id', async (req, res, next) => {
-    try {
-      const poster = await Poster.findByPk(req.params.id);
-      if(!poster) {
-        let err = new Error('No cats or hats or posters here!');
-        err.status = 404;
-        next(err);
-      } else {
-        res.send(poster);
-      }
-    } catch (err) {
+// route for all posters
+router.get("/", async (req, res, next) => {
+  try {
+    const allPosters = await Poster.findAll();
+    res.send(allPosters);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/:id", async (req, res, next) => {
+  try {
+    const poster = await Poster.findByPk(req.params.id);
+    if (!poster) {
+      let err = new Error("No cats or hats or posters here!");
+      err.status = 404;
       next(err);
+    } else {
+      res.send(poster);
     }
-  });
-
+  } catch (err) {
+    next(err);
+  }
+});
+router.put("/:id", async (req, res, next) => {
+  try {
+    const poster = await Poster.findByPk(req.params.id);
+    poster.quantity--;
+    await poster.save();
+    res.json(poster);
+  } catch (err) {
+    next(err);
+  }
+});
