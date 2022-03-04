96b86704be1641c54100714677a80d2e82080259
LPM <11pm@LPMs-MacBook-Air.local>
2022-03-04 15:26:21 -0500

    still working on the add button

--- client/Routes.js
+++ client/Routes.js
@@ -1,68 +1,67 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import AllPosters from './components/AllPosters';
-import SinglePoster from './components/SinglePoster';
-import OrderConfirmation from './components/OrderConfirmation'
-import { Login, Signup } from './components/AuthForm';
-import Home from './components/Home';
-import { me } from './store';
-import ThankyouPage from './components/ThankyouPage';
-import CartItem from './components/cart';
-
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import AllPosters from "./components/AllPosters";
+import SinglePoster from "./components/SinglePoster";
+import OrderConfirmation from "./components/OrderConfirmation";
+import { Login, Signup } from "./components/AuthForm";
+import Home from "./components/Home";
+import { me } from "./store";
+import ThankyouPage from "./components/ThankyouPage";
+import CartItem from "./components/cart";
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-	componentDidMount() {
-		this.props.loadInitialData();
-	}
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
 
-	render() {
-		const { isLoggedIn } = this.props;
+  render() {
+    const { isLoggedIn } = this.props;
 
-		return (
-			<div>
-				{isLoggedIn ? (
-					<Switch>
-						<Route path='/home' component={Home} />
-						<Route exact path='/posters' component={AllPosters} />
-						<Route exact path='/posters/:id' component={SinglePoster} />
-						<Route exact path='/cart' component={CartItem} />
-						{/* <Redirect to="/home" /> */}
-						<Route path='/checkedOut' component={ThankyouPage}/>
-						<Route path='/orderconfirmation' component={OrderConfirmation}/>
-					</Switch>
-				) : (
-					<Switch>
-						<Route path='/' exact component={Login} />
-						<Route path='/login' component={Login} />
-						<Route path='/signup' component={Signup} />
-					</Switch>
-				)}
-			</div>
-		);
-	}
+    return (
+      <div>
+        {isLoggedIn ? (
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route exact path="/posters" component={AllPosters} />
+            <Route exact path="/posters/:id" component={SinglePoster} />
+            <Route exact path="/cart" component={CartItem} />
+            {/* <Redirect to="/home" /> */}
+            <Route path="/checkedOut" component={ThankyouPage} />
+            <Route path="/orderconfirmation" component={OrderConfirmation} />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        )}
+      </div>
+    );
+  }
 }
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-	return {
-		// Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-		// Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-		isLoggedIn: !!state.auth.id,
-	};
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+    isLoggedIn: !!state.auth.id,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-	return {
-		loadInitialData() {
-			dispatch(me());
-		},
-	};
+  return {
+    loadInitialData() {
+      dispatch(me());
+    },
+  };
 };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
