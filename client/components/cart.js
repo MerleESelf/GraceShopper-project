ada76b32243a8c21cd5fad2334f06018bc072751
Amber Lin <amberlinyq@gmail.com>
2022-03-06 12:30:26 -0500

    render poster data in cart

--- client/components/cart.js
+++ client/components/cart.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { removedPoster } from '../store/cart';
 import RemoveButton from './RemoveButton';
-import {checkOutThunk, fetchCompleteOrder, fetchOpenOrder} from '../store/order'
+import {
+	checkOutThunk,
+	fetchCompleteOrder,
+	fetchOpenOrder,
+} from '../store/order';
 
 const poster1 = {
 	id: 1,
@@ -15,7 +19,6 @@ const poster1 = {
 	imageUrl: 'https://loremflickr.com/320/240',
 };
 
-
 class Cart extends React.Component {
 	constructor() {
 		super();
@@ -24,10 +27,10 @@ class Cart extends React.Component {
 		this.handleDelete = this.handleDelete.bind(this);
 	}
 
-	componentDidMount(){
-		const userId = this.props.match.path.split('/')[2]
-		console.log("this.props.match.path", userId)
-		this.props.loadOpenOrder(userId)
+	componentDidMount() {
+		const userId = this.props.match.path.split('/')[2];
+		console.log('this.props.match.path', userId);
+		this.props.loadOpenOrder(userId);
 	}
 
 	handleDelete(posterId) {
@@ -42,7 +45,8 @@ class Cart extends React.Component {
 		this.props.checkOut();
 	}
 	render() {
-		console.log("this.props",this.props)
+		console.log('this.props', this.props);
+		console.log('this.props.order', this.props.order);
 		// order = this.props.order
 		// cartDetail = this.props.order.cartDetail
 		return (
@@ -50,14 +54,14 @@ class Cart extends React.Component {
 				<h1>Shopping Cart</h1>
 
 				<div className='shopping-cart'>
-					<div className='column-labels'>
+					{/* <div className='column-labels'>
 						<label className='product-details'>{poster1.name}</label>
 						<label className='product-price'>{poster1.price}</label>
-						{/* <label className='product-quantity'>{poster1.quantity}</label> */}
+						<label className='product-quantity'>{poster1.quantity}</label>
 						<label className='product-removal'>Remove</label>
 						<label className='product-line-price'>Total</label>
-					</div>
-					{this.props.postersInCart.map((poster) => {
+					</div> */}
+					{/* {this.props.postersInCart.map((poster) => {
 						return (
 							<div className='product' key={poster.id}>
 								<div className='product-image'>
@@ -77,7 +81,31 @@ class Cart extends React.Component {
 								<div className='product-line-price'>25.98</div>
 							</div>
 						);
-					})}
+					})} */}
+					{this.props.order?.cartPosters &&
+						this.props.order.cartPosters.map((orderDetail) => {
+							return (
+								<div key={orderDetail.id}>
+									<div>
+										<img src={orderDetail.posters[0].imageUrl} />
+									</div>
+									<div>Poster Name: {orderDetail.posters[0].name} </div>
+									<div>Poster Size: {orderDetail.posters[0].size} </div>
+									<div>Poster Creator: {orderDetail.posters[0].creator} </div>
+									<div>
+										Poster Description: {orderDetail.posters[0].description}
+									</div>
+									<div>Poster Price: ${orderDetail.posters[0].price} </div>
+									
+									<div>
+										<button>Remove</button>
+									</div>
+									<br />
+									<div></div>
+									<div></div>
+								</div>
+							);
+						})}
 				</div>
 				<form>
 					<button value='submit' onSubmit={this.handleSubmit}>
@@ -91,13 +119,13 @@ class Cart extends React.Component {
 
 const mapStateToProps = (state) => ({
 	postersInCart: state.cart.cart.posters,
-	order: state.order
+	order: state.order,
 });
 
 const mapDispatchToProps = (dispatch) => ({
 	loadOpenOrder: (userId) => dispatch(fetchOpenOrder(userId)),
 	removeAPoster: (posterId) => dispatch(removedPoster(posterId)),
-	checkOut: (orderId) => dispatch(checkOutThunk(orderId))
+	checkOut: (orderId) => dispatch(checkOutThunk(orderId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
